90340d3a95c65388797274ec61ba39685688fd51
Dickson <folidickson78@gmail.com>
2023-07-18 09:32:56 -0700

    4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio>
+/**
+ * main - a program that prints exactly a sentence but returns zero
+ * Return: 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
