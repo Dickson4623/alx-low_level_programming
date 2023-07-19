d72591e1d8b980a6c47525af0dddfe2a0661d643
Dickson <folidickson78@gmail.com>
2023-07-19 07:13:59 -0700

    update quote file

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
-/*
- * main - a program that prints a line to standard error
+/**
+ * main - A program that prints a line to standard error
  * Return: 1
  */
 int main(void)
